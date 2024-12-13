1f9565f64947598a0fa8bd0f25b2ac86b404b761
odhiambo-ed <odhiambo.edward111@gmail.com>
2024-12-13 16:19:22 +0300

    add display popup

--- Scripts/index.js
+++ Scripts/index.js
@@ -3,3 +3,15 @@ function toggleMobileMenu() {
     const navLinks = document.getElementById('navLink');
     navLinks.classList.toggle('active');
 }
+
+// Open the Popup
+function openPopup(title, description) {
+    const popUp = document.getElementById('detailsPopup');
+    const popTitle = document.getElementById('popupTitle');
+    const popDescription = document.getElementById('popupDescription');
+
+    popTitle.textContent = title;
+    popDescription.textContent = description;
+    popUp.style.display = 'flex';
+}
+
